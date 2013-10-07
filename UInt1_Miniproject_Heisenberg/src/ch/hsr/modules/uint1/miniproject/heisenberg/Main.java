452a3decb91f16d0ebe1d70b7c896a372e54385d
msyfrig <Marco@Marco-Laptop>
2013-10-07 12:52:16 +0200

    test commit

---
+++ UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/miniproject/heisenberg/Main.java
@@ -0,0 +1,28 @@
+package ch.hsr.modules.uint1.miniproject.heisenberg;
+
+/**
+ * TODO COMMENT ME!
+ *
+ * @author msyfrig
+ */
+public class Main {
+
+    /**
+     * Creates a new instance of this class.
+     *
+     */
+    public Main() {
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * TODO COMMENT ME!
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
