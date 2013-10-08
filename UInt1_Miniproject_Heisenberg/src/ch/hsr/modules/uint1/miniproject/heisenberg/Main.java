b6979c56ccc4ecc741e8b9e5e58035c456a407cd
theowinter <theo.winter@gmail.com>
2013-10-08 14:00:37 +0200

    test commit

--- UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/miniproject/heisenberg/Main.java
+++ UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/miniproject/heisenberg/Main.java
@@ -2,14 +2,14 @@ package ch.hsr.modules.uint1.miniproject.heisenberg;
 
 /**
  * TODO COMMENT ME!
- *
+ * 
  * @author msyfrig
  */
 public class Main {
 
     /**
      * Creates a new instance of this class.
-     *
+     * 
      */
     public Main() {
         // TODO Auto-generated constructor stub
@@ -17,15 +17,15 @@ public class Main {
 
     /**
      * TODO COMMENT ME!
-     *
+     * 
      * @param args
      */
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-    	System.out.println("blah blubb 2");
+        System.out.println("blah blubb 2");
     }
-    
+
     public static void cookMeth() {
-    	System.out.println("cooking meth..");
+        System.out.println("cooking meth..");
     }
 }
