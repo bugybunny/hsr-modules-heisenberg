034f4ad5e9ecfc1abeb80dcab32ffb80a332bc38
msyfrig <Marco@Marco-Laptop>
2013-10-14 00:15:44 +0200

    support for multiselection of books, opening detailview for all of them and check if a detaildialog is already open

--- UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/heisenberglibrary/domain/book/BookDO.java
+++ UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/heisenberglibrary/domain/book/BookDO.java
@@ -95,4 +95,65 @@ public class BookDO extends AbstractObservableDO {
         return title + ", " + author + ", " + publisher;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((author == null) ? 0 : author.hashCode());
+        result = prime * result
+                + ((publisher == null) ? 0 : publisher.hashCode());
+        result = prime * result + ((shelf == null) ? 0 : shelf.hashCode());
+        result = prime * result + ((title == null) ? 0 : title.hashCode());
+        return result;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        BookDO other = (BookDO) obj;
+        if (author == null) {
+            if (other.author != null) {
+                return false;
+            }
+        } else if (!author.equals(other.author)) {
+            return false;
+        }
+        if (publisher == null) {
+            if (other.publisher != null) {
+                return false;
+            }
+        } else if (!publisher.equals(other.publisher)) {
+            return false;
+        }
+        if (shelf != other.shelf) {
+            return false;
+        }
+        if (title == null) {
+            if (other.title != null) {
+                return false;
+            }
+        } else if (!title.equals(other.title)) {
+            return false;
+        }
+        return true;
+    }
+
 }
--- UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/heisenberglibrary/view/BookMasterJFrame.java
+++ UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/heisenberglibrary/view/BookMasterJFrame.java
@@ -3,10 +3,13 @@ package ch.hsr.modules.uint1.heisenberglibrary.view;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JPanel;
@@ -15,11 +18,12 @@ import javax.swing.border.EmptyBorder;
 import ch.hsr.modules.uint1.heisenberglibrary.domain.book.BookDO;
 
 public class BookMasterJFrame extends JFrame implements Observer {
-    private static final long serialVersionUID = 8186612854405487707L;
+    private static final long    serialVersionUID  = 8186612854405487707L;
 
-    private JPanel            contentPane;
-    private JPanel            centerPane;
-    public JList<BookDO>      bookJList;
+    private JPanel               contentPane;
+    private JPanel               centerPane;
+    public JList<BookDO>         bookJList;
+    private Map<BookDO, JDialog> openBookDialogMap = new HashMap<>();
 
     /**
      * Create the frame.
@@ -43,9 +47,24 @@ public class BookMasterJFrame extends JFrame implements Observer {
         editButtonMasterList.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                BookDetailJDialog frame = new BookDetailJDialog(
-                        BookMasterJFrame.this, bookJList.getSelectedValue());
-                frame.setVisible(true);
+                for (BookDO tempBook : bookJList.getSelectedValuesList()) {
+                    System.out.println(tempBook.getTitle());
+                    // TODO arrange the dialogs on the screen
+                    // check first if the dialog for this dialog is already open
+                    // if so, bring it to the top and focus it
+                    JDialog bookDetailDialog = openBookDialogMap.get(tempBook);
+                    if (bookDetailDialog != null) {
+                        openBookDialogMap.get(tempBook).toFront();
+                    } else {
+                        bookDetailDialog = new BookDetailJDialog(
+                                BookMasterJFrame.this, tempBook);
+                        bookDetailDialog.setVisible(true);
+                        openBookDialogMap.put(tempBook, bookDetailDialog);
+
+                    }
+
+                }
+
             }
         });
         bottomPane.add(editButtonMasterList);
