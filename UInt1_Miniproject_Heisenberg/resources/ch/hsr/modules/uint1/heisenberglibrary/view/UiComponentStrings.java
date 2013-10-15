a39c29be77c3337a8c8160f4d637829778ee3aa8
msyfrig <Marco@Marco-Laptop>
2013-10-15 15:41:42 +0200

    i18n finally (fuck you egit for windows)

---
+++ UInt1_Miniproject_Heisenberg/resources/ch/hsr/modules/uint1/heisenberglibrary/view/UiComponentStrings.java
@@ -0,0 +1,42 @@
+/*
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.hsr.modules.uint1.heisenberglibrary.view;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * TODO COMMENT ME!
+ *
+ * @author msyfrig
+ */
+public class UiComponentStrings {
+    private static final String         BUNDLE_NAME     = "ch.hsr.modules.uint1.heisenberglibrary.view.UiComponentStrings"; //$NON-NLS-1$
+
+    private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+                                                                .getBundle(BUNDLE_NAME);
+
+    private UiComponentStrings() {
+    }
+
+    public static String getString(String key) {
+        try {
+            return RESOURCE_BUNDLE.getString(key);
+        }
+        catch (MissingResourceException e) {
+            return '!' + key + '!';
+        }
+    }
+}
