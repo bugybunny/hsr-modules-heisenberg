313364f59888343d7b6dddfb9bf6156bed7d427d
TheoWinter <theo.winter@gmail.com>
2013-10-17 10:13:51 +0200

    Externalized BookTableColumnName strings of BookTableModel.

--- UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/heisenberglibrary/domain/BookTableModel.java
+++ UInt1_Miniproject_Heisenberg/src/ch/hsr/modules/uint1/heisenberglibrary/domain/BookTableModel.java
@@ -5,31 +5,38 @@ import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
 import ch.hsr.modules.uint1.heisenberglibrary.domain.book.BookDO;
+import ch.hsr.modules.uint1.heisenberglibrary.view.UiComponentStrings;
 
 /**
- * Lists all Books in a jTable with Rows: Available, Title, Author and Publisher.
+ * Lists all Books in a jTable with Rows: Available, Title, Author and
+ * Publisher.
  * 
  * @author twinter
  */
 public class BookTableModel extends AbstractTableModel {
 
 	private static final long serialVersionUID = 4449419618706874102L;
-	private String[] columnNames = {"Available", "Title", "Author", "Publisher"};
-	//private Object[][] data = {{}}; //todo
-	//Test
+	private String[] columnNames = {
+			UiComponentStrings
+					.getString("BookTableModel.bookTableColumn.available"),  //$NON-NLS-1$
+					UiComponentStrings.getString("BookTableModel.bookTableColumn.title"), //$NON-NLS-1$
+					UiComponentStrings.getString("BookTableModel.bookTableColumn.author"),  //$NON-NLS-1$
+					UiComponentStrings.getString("BookTableModel.bookTableColumn.publisher") };  //$NON-NLS-1$
+	// private Object[][] data = {{}}; //todo
+	// Test
 	private List<BookDO> data;
-	
-	public BookTableModel (List<BookDO> someBooks){
+
+	public BookTableModel(List<BookDO> someBooks) {
 		data = someBooks;
-		
-		
 	}
 
 	public void getBooksForTable() {
-		
+
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.table.TableModel#getRowCount()
 	 */
 	@Override
@@ -37,7 +44,9 @@ public class BookTableModel extends AbstractTableModel {
 		return data.size();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.table.TableModel#getColumnCount()
 	 */
 	@Override
@@ -46,63 +55,79 @@ public class BookTableModel extends AbstractTableModel {
 		return columnNames.length;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.table.TableModel#getValueAt(int, int)
 	 */
-	
+
 	// TODO: specific books with availability
 	@Override
 	public Object getValueAt(int aRowIndex, int aColumnIndex) {
 		BookDO book = data.get(aRowIndex);
-		
-		switch (aColumnIndex){		
-			case 0: return "1 (todo)";
-			case 1: return book.getTitle();
-			case 2: return book.getAuthor();
-			case 3: return book.getPublisher();	 
+
+		switch (aColumnIndex) {
+		case 0:
+			return "1 (todo)"; //$NON-NLS-1$
+		case 1:
+			return book.getTitle();
+		case 2:
+			return book.getAuthor();
+		case 3:
+			return book.getPublisher();
 		}
-				
+
 		return data;
 	}
-	
-	public void setValueAt(Object value, int aRowIndex, int aColumnIndex) {        
+
+	public void setValueAt(Object value, int aRowIndex, int aColumnIndex) {
 		BookDO book = data.get(aRowIndex);
-		
-		switch (aColumnIndex){
-			case 0: break; //TODO: availability
-			case 1: book.setTitle((String) value);
-			case 2: book.setAuthor((String) value);
-			case 3: book.setPublisher((String) value);
+
+		switch (aColumnIndex) {
+		case 0:
+			break; // TODO: availability
+		case 1:
+			book.setTitle((String) value);
+		case 2:
+			book.setAuthor((String) value);
+		case 3:
+			book.setPublisher((String) value);
 		}
-      fireTableCellUpdated(aRowIndex, aColumnIndex);
-    }
-	
-	/* (non-Javadoc)
+		fireTableCellUpdated(aRowIndex, aColumnIndex);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.table.AbstractTableModel#getColumnName(int)
 	 */
 	@Override
 	public String getColumnName(int aColumn) {
 		// TODO Auto-generated method stub
-			return columnNames[aColumn];
+		return columnNames[aColumn];
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see javax.swing.table.AbstractTableModel#getColumnClass(int)
 	 */
 	@Override
 	public Class<?> getColumnClass(int aColumnIndex) {
 		// TODO Auto-generated method stub
-		
+
 		switch (aColumnIndex) {
-		case 0: return String.class;
-		case 1: return String.class;
-		case 2: return String.class;
-		case 3: return String.class;
+		case 0:
+			return String.class;
+		case 1:
+			return String.class;
+		case 2:
+			return String.class;
+		case 3:
+			return String.class;
 		}
-		//default
+		// default
 		return String.class;
 	}
-	
-	
-	
+
 }
